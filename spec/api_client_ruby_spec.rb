c1d77df0e7909c376b0a7481c7de5a52163c0acf
Juan Couso <juan.couso@gmail.com>
2018-01-25 15:19:15 -0200

    Test for Simple Measurment

--- spec/api_client_ruby_spec.rb
+++ spec/api_client_ruby_spec.rb
@@ -11,6 +11,45 @@ RSpec.describe ApiClientRuby do
   end
 
   let(:dummy_config) { Config.new(nil, nil, nil)}
+
+
+  context "with Simple Measurements Attributes and JSON" do
+    let(:simple_measurement) { SimpleMeasurement.new }
+
+    it "should set and return id attibute" do
+      id = "meter-01"
+      simple_measurement.id(id)
+      expect(simple_measurement.id).to be_equal id
+    end
+
+    it "should set and return id attibute" do
+      value = rand
+      simple_measurement.value(value)
+      expect(simple_measurement.id).to be_equal value
+    end
+
+    it "should set and return id attibute" do
+      time = Time.now
+      simple_measurement.timestamp(time)
+      expect(simple_measurement.id).to be_equal time
+    end
+
+    it "should return correct JSON" do
+      id = "meter-01"
+      value = rand
+      time = Time.now
+      simple_measurement.id(id)
+      simple_measurement.value(value)
+      simple_measurement.timestamp(time)
+      json = "{id: #{simple_measurement.id},
+              tsISO8601: #{simple_measurement.timestamp},
+              value: #{value}
+              }"
+      expect(simple_measurement.json).to be_equal json
+    end
+
+  end
+
   context "Measurements" do
     it "should send all" do
       countDownLatch = Concurrent::CountDownLatch.new(24)
